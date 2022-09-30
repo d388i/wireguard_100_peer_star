c74dff1fd645f6bfbfbba897a36a293ca8bd1d65
Velizariy Sannikov <97031518+d38i@users.noreply.github.com>
2022-09-30 15:24:15 +0400

    Optimize code

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 echo -e "UPDATE START!"
-sleep 1
-sudo apt -y update && sudo apt -y full-upgrade
-echo -e "UPDATE COMPLETED!"
+sudo apt -y update && sudo apt -y full-upgrade && echo -e "UPDATE COMPLETED!"
