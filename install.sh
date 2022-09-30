26879d41c07c93863a8c1995bef1e69a20b69b86
Velizariy Sannikov <97031518+d38i@users.noreply.github.com>
2022-09-30 15:23:11 +0400

    rm sleep

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 echo "Start Install Wireguard Docker_COMPOSE"
-sleep 1
 sudo chmod -R +x Scripts/
 ./Scripts/update.sh
 ./Scripts/docker.sh
 ./Scripts/wireguard.sh
 echo "Completed Install Wireguard Docker-compose"
-sleep 1
