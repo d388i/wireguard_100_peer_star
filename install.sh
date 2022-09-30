cb7d4254316f5546302473e423cf6f46e52ec3f2
Velizariy Sannikov <97031518+d38i@users.noreply.github.com>
2022-09-30 15:25:59 +0400

    optimize code

--- install.sh
+++ install.sh
@@ -3,5 +3,4 @@ echo "Start Install Wireguard Docker_COMPOSE"
 sudo chmod -R +x Scripts/
 ./Scripts/update.sh
 ./Scripts/docker.sh
-./Scripts/wireguard.sh
-echo "Completed Install Wireguard Docker-compose"
+./Scripts/wireguard.sh && echo "Completed Install Wireguard Docker-compose"
